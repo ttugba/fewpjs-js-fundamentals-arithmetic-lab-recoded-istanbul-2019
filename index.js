afd03271af60d97b857bdbd9242249b34664d38c
tugba <ttugbasen@gmail.com>
2020-05-08 03:11:04 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+var multiply = (num1 * num2)
+
+var random = (Math.random() * 10) + 1;
+
+var mod = 19 % 5
+
+var max = Math.max(14, 17, 2, 9, 5, 20);
